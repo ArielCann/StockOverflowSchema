1f8e146eec676cbebb338b0512315f710464de15
ArielCann <20acann@gmail.com>
2024-10-28 17:41:36 -0400

    updated Schemas.js to use object references where possible.

--- Schemas.js
+++ Schemas.js
@@ -5,28 +5,54 @@ const  messageSchema = new Schema({
         type: Number,
         required: true,
     },
-    UserID: Number,
-    date: Date,
+    text: String,
+    UserID: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+    },
+    date_created: {
+        type: Date,
+        required: true
+    },
+    likes: Number,
+    dislikes: Number,
+    isQuery: {
+        type: Boolean,
+        required: true
+    },
     RepliedTo: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Message',
     },
-    Replies: [messageSchema]
+    Replies: [{type: mongoose.Schema.Types.ObjectId, ref: 'Message'}],
 })
 const userSchema = new Schema({
-    UserID: Number,
+    UserID: {
+        type: Number,
+        required: true
+    },
     email: {
         type:String,
         required:true
     },
     ProfileDesc: String,
-    Password: String,
-    Username: String,
-    Signup: Date,
+    Password: {
+        type: String,
+        required: true
+    },
+    Username: {
+        type: String,
+        required: true
+    },
+    Signup: {
+        type: Date,
+        required: true
+    },
 })
 const stockSchema = new Schema({
     UserID: {
-        type: Number,
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
         required: true
     },
     Ticker: {
