7e0559dc6dd7dad491889832de581d736d21bd20
ArielCann <20acann@gmail.com>
2024-11-18 15:37:40 -0500

    augumented session data interface

--- controllers/PublicForumControllers.ts
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose";
-import express, { Request, Response } from "express";
-import session from "express-session";
-exports.postResponse = (req: Request, res: Response) => {
-    var user = (req.session);
-}
