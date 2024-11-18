464be9dac91ae614cedba31e51b86ae75ed8274f
ArielCann <20acann@gmail.com>
2024-11-18 16:39:24 -0500

    removed user-defined IDs

--- controllers/PublicForumControllers.ts
+++ controllers/PublicForumControllers.ts
@@ -7,12 +7,27 @@ exports.postResponse = async (req: Request, res: Response) => {
     const account = await Account.findOne({AccountID: req.session.AccountID});
     const text = req.body.text;
     const question = await Message.findOne({MessageID: req.body.QuestionID})
-    var msg = await Message.create({
+    const msg = await Message.create({
         text: text,
         Account: account?._id,
-        IsQuery: false,
+        IsQuestion: false,
         Date_Created: new Date(),
         RepliedTo: question?._id,
+        Replies: [],
+        Likes: 0,
+        Dislikes: 0
+    });
+    msg.save();
+}
+exports.postQuestion = async (req: Request, res: Response) => {
+    const account = await Account.findOne({AccountID: req.session.AccountID});
+    const text = req.body.text;
+    const msg = await Message.create({
+        text: text,
+        Account: account?._id,
+        IsQuestion: true,
+        Date_Created: new Date(),
+        RepliedTo: null,
         Replies: [],
         Likes: 0,
         Dislikes: 0
