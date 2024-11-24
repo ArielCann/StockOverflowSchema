94af65750e22ae2af2ea7800b24b86bbfebffe0e
ArielCann <20acann@gmail.com>
2024-11-24 18:16:56 -0500

    copied over messageSchema changes.

--- models/messageSchema.ts
+++ models/messageSchema.ts
@@ -1,21 +1,33 @@
 import mongoose from "mongoose";
-const  messageSchema = new mongoose.Schema({
-    MessageID: {
-        type: Number,
-        required: true,
-    },
+interface IMessage extends Document {
+    _id: mongoose.Types.ObjectId;
+    Text: string;
+    Account: mongoose.Types.ObjectId;
+    Date_Created: Date;
+    Likes: Number;
+    Dislikes: Number;
+    IsQuestion: Boolean;
+    RepliedTo: mongoose.Types.ObjectId;
+    Replies: Array<mongoose.Types.ObjectId>;
+    like(): Boolean;
+    dislike(): Boolean;
+    unlike(): Boolean;
+    un_dislike(): Boolean;
+    addReply(message: mongoose.Types.ObjectId): Boolean;
+}
+const  messageSchema = new mongoose.Schema<IMessage>({
     Text: String,
-    UserID: {
+    Account: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
+        ref: 'Account',
     },
-    Date_created: {
+    Date_Created: {
         type: Date,
         required: true
     },
     Likes: Number,
     Dislikes: Number,
-    IsQuery: {
+    IsQuestion: {
         type: Boolean,
         required: true
     },
@@ -27,9 +39,29 @@ const  messageSchema = new mongoose.Schema({
 })
 messageSchema.methods.like = function(){
     this.Likes += 1;
+    this.save();
 }
+messageSchema.methods.unlike = function(){
+    this.Likes -= 1;
+    this.save();
+}
+
 messageSchema.methods.dislike = function(){
     this.Dislikes += 1;
+    this.save();
+}
+messageSchema.methods.un_dislike = function(){
+    this.Dislikes -= 1;
+    this.save();
+}
+messageSchema.methods.addReply = function(reply_id: mongoose.Schema.Types.ObjectId){
+    const oldSize = this.Replies.length;
+    this.Replies.addToSet([reply_id]);
+    if(this.Replies.length > oldSize){
+        this.save();
+        return true;
+    }
+    return false;
 }
 const Message = mongoose.model("Message", messageSchema);
 export default Message;
